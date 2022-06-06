4d754f0af687902f7b5aae0fd042e9059efea253
Илья Нестерчук <lutinik@gmail.com>
2022-06-06 18:38:21 +0200

    initial commit

---
+++ src/modules/Tasks/Tasks.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button } from 'components/atoms'
+
+const Tasks = () => (
+  <div>
+    <h1>Processes</h1>
+    <Button>Create</Button>
+  </div>
+)
+
+export default Tasks
